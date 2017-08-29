df2308b1f5807136c0adc6f4138043d37c8c09b0
Richard Xu <xxu@huskers.unl.edu>
2017-08-29 12:56:16 -0500

    version 4.5

--- compile_cv_xxu.sh
+++ compile_cv_xxu.sh
@@ -2,8 +2,15 @@
 # This script comiples the cv_xxu.tex file
 # A version number need to be input
 
+current_version=4.5  # 2017.08.27
+
 echo "Type the version of current CV followed by [ENTER]:"
 read version
+
+if [[ $version = "" ]]; then
+  version=$current_version
+fi
+
 echo "Generate version:" $version
 
 # Compile the tex file
